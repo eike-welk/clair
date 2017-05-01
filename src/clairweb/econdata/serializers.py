e0c2931e057f59f27eee55c9c514281009e7e69c
Eike Welk <eike.welk@gmx.net>
2017-05-01 22:39:25 +0200

    Added REST API

---
+++ src/clairweb/econdata/serializers.py
@@ -0,0 +1,27 @@
+from .models import Listing, Product, Price
+from rest_framework import serializers
+
+
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Listing
+        fields = (
+            'id', 'site', 'id_site', 
+            'title', 'description', 'prod_spec', 'condition', 
+            'time', 'currency', 'price', 'shipping_price', 'is_real', 'is_sold',
+            'location', 'shipping_locations', 'seller', 'buyer', 'item_url', 
+            'status', 'listing_type',)
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'important_words', 'categories', 'description',
+                  'description_url1', 'description_url2',)
+
+class PriceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Price
+        fields = ('id', 'price', 'currency', 'condition', 'time', 'product', 
+                  'listing', 'price_type', 'is_sold', 'avg_period', 
+                  'avg_num_listings',)
+
