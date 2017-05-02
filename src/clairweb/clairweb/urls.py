ffeab7aed9afc4f5b937171a1615cfb094065a03
Eike Welk <eike.welk@gmx.net>
2017-05-02 20:16:08 +0200

    Create very simple homepage.

--- src/clairweb/clairweb/urls.py
+++ src/clairweb/clairweb/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from . import views
+
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^econdata/', include('econdata.urls')),
     url(r'^collect/', include('collect.urls')),
     url(r'^admin/', admin.site.urls),
