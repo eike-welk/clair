5708410cfdfdd5a86b726e98cd096b9ed24405d0
Eike Welk <eike.welk@gmx.net>
2017-10-05 01:38:06 +0200

    Small progress in details view for Search Tasks.

--- src/clairweb/collect/views.py
+++ src/clairweb/collect/views.py
@@ -13,6 +13,9 @@ def index(request):
 def searchTasks(request):
     return render(request, 'collect/search-tasks.html', {})
 
+def search_task_details(request, search_task_id):
+    print('search-task_id: ', search_task_id)
+    task = SearchTask.objects.get(pk=search_task_id)
 
 # API -------------------------------------------------------------------------
 class SearchTaskViewSet(viewsets.ModelViewSet):
