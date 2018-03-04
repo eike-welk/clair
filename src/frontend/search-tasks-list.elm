47c45131a28c307c96e62d861749f9112f83b327
Eike Welk <eike.welk@gmx.net>
2018-03-04 22:27:08 +0100

    Parsing whole record, different parsing library.

--- src/frontend/search-tasks-list.elm
+++ src/frontend/search-tasks-list.elm
@@ -4,7 +4,8 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode as Decode
+import Json.Decode as JD
+import Json.Decode.Pipeline as PL
 import Debug exposing (log)
 
 
@@ -22,10 +23,16 @@ main =
 
 
 type alias SearchTask =
-    { id : String
+    { expanded : Bool
+    , id : String
     , queryString : String
-
-    --, search_words : String
+    , recurrence : String
+    , server : String
+    , product : Maybe String
+    , nListings : Int
+    , priceMin : Float
+    , priceMax : Float
+    , currency : String
     }
 
 
@@ -37,8 +44,8 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model [ SearchTask "1" "foo", SearchTask "2" "bar" ] ""
-    , Cmd.none
+    ( Model [] ""
+    , getTaskList
     )
 
 
@@ -47,18 +54,18 @@ init =
 
 
 type Msg
-    = MorePlease
+    = ReloadTasks
     | NewTasks (Result Http.Error (List SearchTask))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        MorePlease ->
-            ( model, getTaskList )
+        ReloadTasks ->
+            ( { model | tasks = [] }, getTaskList )
 
         NewTasks (Ok newTasks) ->
-            ( { model | tasks = newTasks }, Cmd.none )
+            ( { model | tasks = List.append model.tasks newTasks }, Cmd.none )
 
         NewTasks (Err err) ->
             ( { model | error = (toString err) }, Cmd.none )
@@ -77,13 +84,20 @@ view model =
             [ thead []
                 [ tr []
                     [ th [] [ text "ID" ]
+                    , th [] [ text "Product" ]
                     , th [] [ text "Query String" ]
+                    , th [] [ text "Server" ]
+                    , th [] [ text "Recurrence" ]
+                    , th [] [ text "Number Listings" ]
+                    , th [] [ text "Price Min" ]
+                    , th [] [ text "Price Max" ]
+                    , th [] [ text "Currency" ]
                     ]
                 ]
             , tbody []
                 (List.map viewSearchTask model.tasks)
             ]
-        , button [ onClick MorePlease ] [ text "More Please!" ]
+        , viewError model
         ]
 
 
@@ -91,8 +105,29 @@ viewSearchTask : SearchTask -> Html Msg
 viewSearchTask task =
     tr []
         [ td [] [ text task.id ]
+        , td [] [ text "-" ] --"product": null,
         , td [] [ text task.queryString ]
+        , td [] [ text task.server ]
+        , td [] [ text task.recurrence ]
+        , td [] [ text (toString task.nListings) ]
+        , td [] [ text (toString task.priceMin) ]
+        , td [] [ text (toString task.priceMax) ]
+        , td [] [ text task.currency ]
         ]
+
+
+viewError : Model -> Html Msg
+viewError model =
+    if model.error == "" then
+        div [] []
+    else
+        div
+            [ style
+                [ ( "color", "red" )
+                , ( "font-weight", "bold" )
+                ]
+            ]
+            [ text ("Error: " ++ model.error) ]
 
 
 
@@ -108,26 +143,60 @@ subscriptions model =
 -- HTTP
 
 
+{-| The API URL for accessing search tasks.
+-}
+url_SEARCH_TASKS : String
+url_SEARCH_TASKS =
+    --"/collect/api/search_tasks/"
+    "http://localhost:8000/collect/api/search_tasks/"
+
+
 getTaskList : Cmd Msg
 getTaskList =
-    let
-        url =
-            "http://localhost:8000/collect/api/search_tasks/"
-    in
-        Http.send NewTasks (Http.get url decodeGifUrl)
+    Http.send NewTasks (Http.get url_SEARCH_TASKS parseSearchTaskList)
 
 
-decodeGifUrl : Decode.Decoder (List SearchTask)
-decodeGifUrl =
-    Decode.at [ "results" ]
-        (Decode.list
-            (Decode.map2 makeSearchTask
-                (Decode.at [ "id" ] Decode.int)
-                (Decode.at [ "query_string" ] Decode.string)
+{-| Parse the JSON response from the "Search Tasks" API.
+
+The JSON has the following form:
+
+    """
+    {
+        "count": 2,
+        "next": null,
+        "previous": null,
+        "results": [
+            {
+                "id": 1,
+                "recurrence": "3 00:00:00",
+                "server": "EBAY-DE",
+                "product": null,
+                "query_string": "Nikon D90",
+                "n_listings": 50,
+                "price_min": 100.0,
+                "price_max": 500.0,
+                "currency": "EUR"
+            },
+            {...}
+        ]
+    }
+    """
+
+-}
+parseSearchTaskList : JD.Decoder (List SearchTask)
+parseSearchTaskList =
+    JD.at [ "results" ]
+        (JD.list
+            (PL.decode SearchTask
+                |> PL.hardcoded False
+                |> PL.required "id" (JD.map toString JD.int)
+                |> PL.required "query_string" JD.string
+                |> PL.required "recurrence" JD.string
+                |> PL.required "server" JD.string
+                |> PL.required "product" (JD.nullable JD.string)
+                |> PL.required "n_listings" JD.int
+                |> PL.required "price_min" JD.float
+                |> PL.required "price_max" JD.float
+                |> PL.required "currency" JD.string
             )
         )
-
-
-makeSearchTask : Int -> String -> SearchTask
-makeSearchTask id queryString =
-    SearchTask (log "id" (toString id)) (log "queryString" queryString)
